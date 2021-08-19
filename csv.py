ac363938f17c8a2541245ad5cb06e47096ce177d
anzhelaiondem <anzhlela.iondem.fd@gmail.com>
2021-08-19 18:49:08 +0400

    "if" statement checks

--- csv.py
+++ csv.py
@@ -1,2 +1,31 @@
 file_name = "database.db"
-print(file_name)
+
+
+# print(file_name)
+
+
+def is_file(a):
+    if a > 10:
+        print("File Exists")
+        return True
+    else:
+        print("file doesn't exist")
+        return False
+
+
+def is_table(b):
+    if b > 10:
+        print("Table Exists")
+        return True
+    else:
+        print("Table doesn't exist")
+        return False
+
+
+a = is_file(0)
+b = is_table(0)
+
+if a and b:
+    print("Both file and table")
+else:
+    print("File and/or table doesn't exist")
